1acf76a86f1276b153a5cfef67981219cdbc425d
Magarusik <mrasul13112001@gmail.com>
2023-12-11 11:25:34 +0300

    init commit

---
+++ src/main/java/ru/magarusik/studenttester/StudentTesterApplication.java
@@ -0,0 +1,13 @@
+package ru.magarusik.studenttester;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentTesterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentTesterApplication.class, args);
+    }
+
+}
