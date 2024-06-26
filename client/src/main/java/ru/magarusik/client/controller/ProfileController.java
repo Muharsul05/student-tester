557f7145f519a105589d24cacaf1a5bbb8fb6849
Magarusik <mrasul13112001@gmail.com>
2024-06-26 18:01:07 +0300

    profile update in user list

--- client/src/main/java/ru/magarusik/client/controller/ProfileController.java
+++ client/src/main/java/ru/magarusik/client/controller/ProfileController.java
@@ -5,6 +5,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import ru.magarusik.client.client.RestClientTesting;
 import ru.magarusik.client.security.ClientUserDetailService;
@@ -20,6 +21,15 @@ public class ProfileController {
     @GetMapping
     public String getProfilePage(Model model, Authentication authentication) {
         var user = clientUserDetailService.findUserByUsername(authentication.getName());
+        var userProfile = restClientTesting.getUserProfile(user.getId());
+        model.addAttribute("user", user);
+        model.addAttribute("userProfile", userProfile);
+        return "/user/profile";
+    }
+
+    @GetMapping("/{username}")
+    public String getProfilePage(Model model, @PathVariable String username) {
+        var user = clientUserDetailService.findUserByUsername(username);
         var userProfile = restClientTesting.getUserProfile(user.getId());
         model.addAttribute("user", user);
         model.addAttribute("userProfile", userProfile);
